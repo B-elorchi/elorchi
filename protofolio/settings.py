e34853a567b8e47dd9507bc91038ce2f53067813
Brahim Elorchi <brahimelorchi1937@gmail.com>
2022-03-17 00:27:49 +0100

    setup debug on false

--- protofolio/settings.py
+++ protofolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ux+-^22^-m=qvlqc@mc#klv#9_9@ck6t$7vzdg=)jf)cg^*3q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'elorchi.herokuapp.com']
 
 
 # Application definition
