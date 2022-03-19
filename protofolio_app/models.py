1164d6a1d7608081df092e4dc04ce9e7f56e2547
Brahim Elorchi <brahimelorchi1937@gmail.com>
2022-03-19 12:54:10 +0100

    add table apps

--- protofolio_app/models.py
+++ protofolio_app/models.py
@@ -38,3 +38,10 @@ class tools(models.Model):
 
     def __str__(self):
         return self.name
+
+class appsM(models.Model):
+    name = models.CharField(max_length=100)
+    teau = models.DecimalField(max_digits=3, decimal_places=1)
+
+    def __str__(self):
+        return self.name
